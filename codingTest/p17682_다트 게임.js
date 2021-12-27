ee7a53d0dddc71d17f0d7b1426784a471c0e5265
wonsss <bigdatajang@gmail.com>
2021-12-27 22:49:57 +0900

    코딩테스트(다트게임) 풀이 완료

--- codingTest/p17682_다트 게임.js
+++ codingTest/p17682_다트 게임.js
@@ -1,15 +1,39 @@
 function solution(dartResult) {
-  const regexNum = /\d/g;
-  const regexEng = /[A-Z]/g;
-  const regexStar = /[\*\#]/g;
-  const idxS = dartResult.indexOf("S");
-  const idxD = dartResult.indexOf("D");
-  const idxT = dartResult.indexOf("T");
-  const numS = dartResult.slice(, idxS);
-  console.log(idxS, idxD, idxT);
-  console.log(numS);
+  const result = new Array(3);
+  let index = -1;
+  for (let i = 0; i < dartResult.length; i++) {
+    switch (true) {
+      case /[\d]/.test(+dartResult[i]):
+        index++;
+        if (+dartResult[i] === 1 && +dartResult[i + 1] === 0) {
+          result[index] = 10;
+          i++;
+        } else {
+          result[index] = +dartResult[i];
+        }
+        break;
+      case dartResult[i] === "S":
+        result[index] **= 1;
+        break;
+      case dartResult[i] === "D":
+        result[index] **= 2;
+        break;
+      case dartResult[i] === "T":
+        result[index] **= 3;
+        break;
+      case dartResult[i] === "*":
+        result[index] *= 2;
+        result[index - 1] *= 2;
+        break;
+      case dartResult[i] === "#":
+        result[index] *= -1;
+        break;
+      default:
+        break;
+    }
+  }
+  return result.reduce((a, b) => a + b);
 }
 
-const dartResult = "10S2D*3T";
-// solution(dartResult);
+const dartResult = "10S#2D*3T";
 console.log(solution(dartResult));
